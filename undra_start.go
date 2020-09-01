74ab735f06f171b2167617c159840d1e39baa9e4
Ankizle <ankizleyt@gmail.com>
2020-09-01 03:39:47 -0400

    updated the usage function

--- undra_start.go
+++ undra_start.go
@@ -4,14 +4,20 @@ import (
 	"flag"
 	"os"
 
+	suite "github.com/omm-lang/omm-suite"
 	"github.com/omm-lang/omm/lang/types"
 	undra "github.com/omm-lang/undra/server"
 )
 
-var cwd = flag.String("cwd", "", "set cwd")
-var host = flag.String("host", "localhost:80", "set cwd")
+var cwd = flag.String("cwd", "", "set the current working directory (automatically placed by the shell/pwsh script)")
+var host = flag.String("host", "localhost:80", "set ")
+
+func init() {
+	flag.Usage = suite.Usagef("Undra")
+}
 
 func main() {
+	flag.Parse()
 
 	basedir, _ := os.Executable()
 	var params types.CliParams
